95f9c3967a667aed21bfa0c8a58f3d7da12d9f0c
deforde <daniel.forde001@gmail.com>
2022-12-01 10:55:57 +0200

    factor out common code

--- src/problem_001.zig
+++ src/problem_001.zig
@@ -1,13 +1,12 @@
 const std = @import("std");
 const readFileIntoBuf = @import("util.zig").readFileIntoBuf;
 
-pub fn part1() anyerror!i32 {
+fn get_sum_list() anyerror!std.ArrayList(i32) {
     const allocator = std.heap.page_allocator;
     const buf = try readFileIntoBuf(allocator, "problems/problem_001.txt");
     var tokens = std.mem.split(u8, buf, "\n");
     var sum: i32 = 0;
     var list = std.ArrayList(i32).init(allocator);
-    defer list.deinit();
     while (tokens.next()) |token| {
         if (token.len == 0) {
             try list.append(sum);
@@ -19,27 +18,18 @@ pub fn part1() anyerror!i32 {
     }
     try list.append(sum);
     std.sort.sort(i32, list.items, {}, std.sort.desc(i32));
+    return list;
+}
+
+pub fn part1() anyerror!i32 {
+    var list = try get_sum_list();
+    defer list.deinit();
     return list.items[0];
 }
 
 pub fn part2() anyerror!i32 {
-    const allocator = std.heap.page_allocator;
-    const buf = try readFileIntoBuf(allocator, "problems/problem_001.txt");
-    var tokens = std.mem.split(u8, buf, "\n");
-    var sum: i32 = 0;
-    var list = std.ArrayList(i32).init(allocator);
+    var list = try get_sum_list();
     defer list.deinit();
-    while (tokens.next()) |token| {
-        if (token.len == 0) {
-            try list.append(sum);
-            sum = 0;
-        } else {
-            const cur = try std.fmt.parseInt(i32, token, 10);
-            sum += cur;
-        }
-    }
-    try list.append(sum);
-    std.sort.sort(i32, list.items, {}, std.sort.desc(i32));
     return list.items[0] + list.items[1] + list.items[2];
 }
 
