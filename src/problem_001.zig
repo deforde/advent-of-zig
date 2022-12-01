96e39f1569db41d229fafa1c04f15a75611807b6
deforde <daniel.forde001@gmail.com>
2022-12-01 08:55:15 +0200

    complete day 1 parts 1 & 2

--- src/problem_001.zig
+++ src/problem_001.zig
@@ -4,55 +4,51 @@ const readFileIntoBuf = @import("util.zig").readFileIntoBuf;
 pub fn part1() anyerror!i32 {
     const allocator = std.heap.page_allocator;
     const buf = try readFileIntoBuf(allocator, "problems/problem_001.txt");
-    var tokens = std.mem.tokenize(u8, buf, "\n");
-    var prev = try std.fmt.parseInt(i32, tokens.next().?, 10);
-    var inc: i32 = 0;
+    var tokens = std.mem.split(u8, buf, "\n");
+    var sum: i32 = 0;
+    var list = std.ArrayList(i32).init(allocator);
+    defer list.deinit();
     while (tokens.next()) |token| {
-        const cur = try std.fmt.parseInt(i32, token, 10);
-        if (cur > prev) {
-            inc += 1;
+        if (token.len == 0) {
+            try list.append(sum);
+            sum = 0;
+        } else {
+            const cur = try std.fmt.parseInt(i32, token, 10);
+            sum += cur;
         }
-        prev = cur;
     }
-    return inc;
+    try list.append(sum);
+    std.sort.sort(i32, list.items, {}, std.sort.desc(i32));
+    return list.items[0];
 }
 
 pub fn part2() anyerror!i32 {
     const allocator = std.heap.page_allocator;
     const buf = try readFileIntoBuf(allocator, "problems/problem_001.txt");
-    var tokens = std.mem.tokenize(u8, buf, "\n");
-    var inc: i32 = 0;
-    var win = [_]i32{ 0, 0, 0 };
-    var idx: i32 = 0;
-    var prev_sum: i32 = 0;
+    var tokens = std.mem.split(u8, buf, "\n");
+    var sum: i32 = 0;
+    var list = std.ArrayList(i32).init(allocator);
+    defer list.deinit();
     while (tokens.next()) |token| {
-        const cur = try std.fmt.parseInt(i32, token, 10);
-
-        var cur_sum: i32 = 0;
-        var i: usize = 1;
-        while (i < win.len) : (i += 1) {
-            win[i - 1] = win[i];
-            cur_sum += win[i - 1];
+        if (token.len == 0) {
+            try list.append(sum);
+            sum = 0;
+        } else {
+            const cur = try std.fmt.parseInt(i32, token, 10);
+            sum += cur;
         }
-        win[win.len - 1] = cur;
-        cur_sum += cur;
-
-        if (idx > 2 and cur_sum > prev_sum) {
-            inc += 1;
-        }
-
-        prev_sum = cur_sum;
-        idx += 1;
     }
-    return inc;
+    try list.append(sum);
+    std.sort.sort(i32, list.items, {}, std.sort.desc(i32));
+    return list.items[0] + list.items[1] + list.items[2];
 }
 
 test "part_001" {
     const ans = try part1();
-    try std.testing.expectEqual(@as(i32, 1583), ans);
+    try std.testing.expectEqual(@as(i32, 67633), ans);
 }
 
 test "part_002" {
     const ans = try part2();
-    try std.testing.expectEqual(@as(i32, 1627), ans);
+    try std.testing.expectEqual(@as(i32, 199628), ans);
 }
