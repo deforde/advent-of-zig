8f19a0f0a9b119e86b8f92eff3b683943796d719
deforde <daniel.forde001@gmail.com>
2022-12-02 11:53:14 +0200

    reduce runtime memory usage and dynamic allocation

--- src/problem_001.zig
+++ src/problem_001.zig
@@ -1,36 +1,43 @@
 const std = @import("std");
 const readFileIntoBuf = @import("util.zig").readFileIntoBuf;
 
-fn getSumList() anyerror!std.ArrayList(i32) {
+fn updateList(list: []i32, sum: i32) void {
+    for (list) |val, idx| {
+        if (sum > val) {
+            list[idx] = sum;
+            break;
+        }
+    }
+}
+
+fn getSumList() anyerror![3]i32 {
     const allocator = std.heap.page_allocator;
     const buf = try readFileIntoBuf(allocator, "problems/problem_001.txt");
     var tokens = std.mem.split(u8, buf, "\n");
     var sum: i32 = 0;
-    var list = std.ArrayList(i32).init(allocator);
+    var list = [3]i32{ 0, 0, 0 };
     while (tokens.next()) |token| {
         if (token.len == 0) {
-            try list.append(sum);
+            updateList(&list, sum);
             sum = 0;
         } else {
             const cur = try std.fmt.parseInt(i32, token, 10);
             sum += cur;
         }
     }
-    try list.append(sum);
-    std.sort.sort(i32, list.items, {}, std.sort.desc(i32));
+    updateList(&list, sum);
+    std.sort.sort(i32, &list, {}, std.sort.desc(i32));
     return list;
 }
 
 fn part1() anyerror!i32 {
     var list = try getSumList();
-    defer list.deinit();
-    return list.items[0];
+    return list[0];
 }
 
 fn part2() anyerror!i32 {
     var list = try getSumList();
-    defer list.deinit();
-    return list.items[0] + list.items[1] + list.items[2];
+    return list[0] + list[1] + list[2];
 }
 
 test "part1" {
