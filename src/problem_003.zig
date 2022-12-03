72a14b4b47a3ce2d9425d6ae1cfd70492f7189d4
deforde <daniel.forde001@gmail.com>
2022-12-03 08:57:29 +0200

    day 3 parts 1 & 2

---
+++ src/problem_003.zig
@@ -0,0 +1,168 @@
+const std = @import("std");
+const readFileIntoBuf = @import("util.zig").readFileIntoBuf;
+
+fn solve1(path: []const u8) anyerror!i32 {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer std.debug.assert(!gpa.deinit());
+
+    const buf = try readFileIntoBuf(allocator, path);
+    defer allocator.free(buf);
+
+    var sum: i32 = 0;
+    var lines = std.mem.tokenize(u8, buf, "\n");
+    while (lines.next()) |line| {
+        const sz: usize = line.len;
+        std.debug.assert(sz % 2 == 0);
+
+        var c1 = std.ArrayList(i32).init(allocator);
+        defer c1.deinit();
+        for (line[0 .. sz / 2]) |char| {
+            if (char > 'Z') {
+                try c1.append(char - 'a' + 1);
+            } else {
+                try c1.append(char - 'A' + 27);
+            }
+        }
+        std.sort.sort(i32, c1.items, {}, comptime std.sort.asc(i32));
+
+        var c2 = std.ArrayList(i32).init(allocator);
+        defer c2.deinit();
+        for (line[sz / 2 ..]) |char| {
+            if (char > 'Z') {
+                try c2.append(char - 'a' + 1);
+            } else {
+                try c2.append(char - 'A' + 27);
+            }
+        }
+        std.sort.sort(i32, c2.items, {}, comptime std.sort.asc(i32));
+
+        var dup: i32 = -1;
+        var idxC1: usize = 0;
+        var idxC2: usize = 0;
+        while (idxC1 < c1.items.len and idxC2 < c2.items.len) {
+            if (c1.items[idxC1] == c2.items[idxC2]) {
+                dup = c1.items[idxC1];
+                break;
+            }
+            if (c1.items[idxC1] > c2.items[idxC2]) {
+                idxC2 += 1;
+            } else {
+                idxC1 += 1;
+            }
+        }
+        std.debug.assert(dup != -1);
+        sum += dup;
+    }
+
+    return sum;
+}
+
+fn solve2(path: []const u8) anyerror!i32 {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer std.debug.assert(!gpa.deinit());
+
+    const buf = try readFileIntoBuf(allocator, path);
+    defer allocator.free(buf);
+
+    var sum: i32 = 0;
+    var lines = std.mem.tokenize(u8, buf, "\n");
+    var group_idx: usize = 0;
+
+    var c1 = std.ArrayList(i32).init(allocator);
+    defer c1.deinit();
+    var c2 = std.ArrayList(i32).init(allocator);
+    defer c2.deinit();
+    var c3 = std.ArrayList(i32).init(allocator);
+    defer c3.deinit();
+
+    var group = [_]*std.ArrayList(i32){
+        &c1,
+        &c2,
+        &c3,
+    };
+
+    while (lines.next()) |line| {
+        var c = group[group_idx];
+        for (line) |char| {
+            if (char > 'Z') {
+                try c.*.append(char - 'a' + 1);
+            } else {
+                try c.*.append(char - 'A' + 27);
+            }
+        }
+        std.sort.sort(i32, c.*.items, {}, comptime std.sort.asc(i32));
+        if (group_idx == 2) {
+            var dup: i32 = -1;
+
+            for (group[0].*.items) |c1_val| {
+                var in_c2 = false;
+                for (group[1].*.items) |c2_val| {
+                    if (c1_val == c2_val) {
+                        in_c2 = true;
+                        break;
+                    }
+                }
+                if (in_c2) {
+                    for (group[2].*.items) |c3_val| {
+                        if (c1_val == c3_val) {
+                            dup = c1_val;
+                            break;
+                        }
+                    }
+                }
+                if (dup != -1) {
+                    break;
+                }
+            }
+            std.debug.assert(dup != -1);
+
+            sum += dup;
+            for (group) |list| {
+                list.*.clearRetainingCapacity();
+            }
+            group_idx = 0;
+            continue;
+        }
+        group_idx += 1;
+    }
+
+    return sum;
+}
+
+fn example1() anyerror!i32 {
+    return solve1("problems/example_003.txt");
+}
+
+fn example2() anyerror!i32 {
+    return solve2("problems/example_003.txt");
+}
+
+fn part1() anyerror!i32 {
+    return solve1("problems/problem_003.txt");
+}
+
+fn part2() anyerror!i32 {
+    return solve2("problems/problem_003.txt");
+}
+
+test "example1" {
+    const ans = try example1();
+    try std.testing.expectEqual(@as(i32, 157), ans);
+}
+
+test "part1" {
+    const ans = try part1();
+    try std.testing.expectEqual(@as(i32, 7990), ans);
+}
+
+test "example2" {
+    const ans = try example2();
+    try std.testing.expectEqual(@as(i32, 70), ans);
+}
+
+test "part2" {
+    const ans = try part2();
+    try std.testing.expectEqual(@as(i32, 2602), ans);
+}
