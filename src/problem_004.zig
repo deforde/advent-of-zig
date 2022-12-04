87de78fa5f83f7fd626875a4251093468e738ccd
deforde <daniel.forde001@gmail.com>
2022-12-04 07:39:42 +0200

    day 4 parts 1 & 2

---
+++ src/problem_004.zig
@@ -0,0 +1,88 @@
+const std = @import("std");
+const readFileIntoBuf = @import("util.zig").readFileIntoBuf;
+
+fn solve1(path: []const u8) anyerror!i32 {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer std.debug.assert(!gpa.deinit());
+
+    const buf = try readFileIntoBuf(allocator, path);
+    defer allocator.free(buf);
+
+    var count: i32 = 0;
+    var lines = std.mem.tokenize(u8, buf, "\n");
+    while (lines.next()) |line| {
+        var values = std.mem.tokenize(u8, line, "-,");
+        const e1_1 = try std.fmt.parseInt(i32, values.next().?, 10);
+        const e1_2 = try std.fmt.parseInt(i32, values.next().?, 10);
+        const e2_1 = try std.fmt.parseInt(i32, values.next().?, 10);
+        const e2_2 = try std.fmt.parseInt(i32, values.next().?, 10);
+        if ((e1_1 >= e2_1 and e1_2 <= e2_2) or (e2_1 >= e1_1 and e2_2 <= e1_2)) {
+            count += 1;
+        }
+    }
+
+    return count;
+}
+
+fn solve2(path: []const u8) anyerror!i32 {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer std.debug.assert(!gpa.deinit());
+
+    const buf = try readFileIntoBuf(allocator, path);
+    defer allocator.free(buf);
+
+    var count: i32 = 0;
+    var lines = std.mem.tokenize(u8, buf, "\n");
+    while (lines.next()) |line| {
+        var values = std.mem.tokenize(u8, line, "-,");
+        const e1_1 = try std.fmt.parseInt(i32, values.next().?, 10);
+        const e1_2 = try std.fmt.parseInt(i32, values.next().?, 10);
+        const e2_1 = try std.fmt.parseInt(i32, values.next().?, 10);
+        const e2_2 = try std.fmt.parseInt(i32, values.next().?, 10);
+        const start = std.math.max(e1_1, e2_1);
+        const end = std.math.min(e1_2, e2_2);
+        if (end >= start) {
+            count += 1;
+        }
+    }
+
+    return count;
+}
+
+fn example1() anyerror!i32 {
+    return solve1("problems/example_004.txt");
+}
+
+fn example2() anyerror!i32 {
+    return solve2("problems/example_004.txt");
+}
+
+fn part1() anyerror!i32 {
+    return solve1("problems/problem_004.txt");
+}
+
+fn part2() anyerror!i32 {
+    return solve2("problems/problem_004.txt");
+}
+
+test "example1" {
+    const ans = try example1();
+    try std.testing.expectEqual(@as(i32, 2), ans);
+}
+
+test "part1" {
+    const ans = try part1();
+    try std.testing.expectEqual(@as(i32, 534), ans);
+}
+
+test "example2" {
+    const ans = try example2();
+    try std.testing.expectEqual(@as(i32, 4), ans);
+}
+
+test "part2" {
+    const ans = try part2();
+    try std.testing.expectEqual(@as(i32, 841), ans);
+}
