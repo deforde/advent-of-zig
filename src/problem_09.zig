9413817eaeec05e09fe6bb41a3323fdec042dc4d
deforde <daniel.forde001@gmail.com>
2022-12-09 09:19:03 +0200

    wip

--- src/problem_09.zig
+++ src/problem_09.zig
@@ -6,9 +6,9 @@ const Coord = struct {
     y: i32 = 0,
 };
 
-fn moveTail(head: Coord, tail: *Coord, tail_positions: *std.ArrayList(Coord)) anyerror!void {
-    var delta_x = head.x - tail.*.x;
-    var delta_y = head.y - tail.*.y;
+fn moveFollower(leader: Coord, follower: *Coord) anyerror!void {
+    var delta_x = leader.x - follower.*.x;
+    var delta_y = leader.y - follower.*.y;
     if (try std.math.absInt(delta_x) <= 1 and try std.math.absInt(delta_y) <= 1) {
         return;
     }
@@ -20,23 +20,25 @@ fn moveTail(head: Coord, tail: *Coord, tail_positions: *std.ArrayList(Coord)) an
         delta_y = @divTrunc(delta_y, try std.math.absInt(delta_y));
     }
 
-    tail.*.x += delta_x;
-    tail.*.y += delta_y;
+    follower.*.x += delta_x;
+    follower.*.y += delta_y;
+}
 
+fn logTailPos(tail: Coord, tail_positions: *std.ArrayList(Coord)) anyerror!void {
     var unique = true;
     for (tail_positions.*.items) |pos| {
-        if (pos.x == tail.*.x and pos.y == tail.*.y) {
+        if (pos.x == tail.x and pos.y == tail.y) {
             unique = false;
             break;
         }
     }
 
     if (unique) {
-        try tail_positions.*.append(tail.*);
+        try tail_positions.*.append(tail);
     }
 }
 
-fn solve(path: []const u8) anyerror!usize {
+fn solve(path: []const u8, nknots: usize) anyerror!usize {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
     defer std.debug.assert(!gpa.deinit());
@@ -44,8 +46,15 @@ fn solve(path: []const u8) anyerror!usize {
     const buf = try readFileIntoBuf(allocator, path);
     defer allocator.free(buf);
 
-    var head = Coord{};
-    var tail = Coord{};
+    var rope = std.ArrayList(Coord).init(allocator);
+    defer rope.deinit();
+    var j: usize = 0;
+    while (j < nknots) : (j += 1) {
+        try rope.append(Coord{});
+    }
+    var head = &rope.items[0];
+    var tail = &rope.items[rope.items.len - 1];
+
     var tail_positions = std.ArrayList(Coord).init(allocator);
     defer tail_positions.deinit();
     try tail_positions.append(Coord{});
@@ -59,29 +68,45 @@ fn solve(path: []const u8) anyerror!usize {
             'U' => {
                 var i: i32 = 0;
                 while (i < cnt) : (i += 1) {
-                    head.y += 1;
-                    try moveTail(head, &tail, &tail_positions);
+                    head.*.y += 1;
+                    var k: usize = 0;
+                    while (k < nknots - 1) : (k += 1) {
+                        try moveFollower(rope.items[k], &rope.items[k + 1]);
+                    }
+                    try logTailPos(tail.*, &tail_positions);
                 }
             },
             'D' => {
                 var i: i32 = 0;
                 while (i < cnt) : (i += 1) {
-                    head.y -= 1;
-                    try moveTail(head, &tail, &tail_positions);
+                    head.*.y -= 1;
+                    var k: usize = 0;
+                    while (k < nknots - 1) : (k += 1) {
+                        try moveFollower(rope.items[k], &rope.items[k + 1]);
+                    }
+                    try logTailPos(tail.*, &tail_positions);
                 }
             },
             'L' => {
                 var i: i32 = 0;
                 while (i < cnt) : (i += 1) {
-                    head.x -= 1;
-                    try moveTail(head, &tail, &tail_positions);
+                    head.*.x -= 1;
+                    var k: usize = 0;
+                    while (k < nknots - 1) : (k += 1) {
+                        try moveFollower(rope.items[k], &rope.items[k + 1]);
+                    }
+                    try logTailPos(tail.*, &tail_positions);
                 }
             },
             'R' => {
                 var i: i32 = 0;
                 while (i < cnt) : (i += 1) {
-                    head.x += 1;
-                    try moveTail(head, &tail, &tail_positions);
+                    head.*.x += 1;
+                    var k: usize = 0;
+                    while (k < nknots - 1) : (k += 1) {
+                        try moveFollower(rope.items[k], &rope.items[k + 1]);
+                    }
+                    try logTailPos(tail.*, &tail_positions);
                 }
             },
             else => unreachable,
@@ -93,11 +118,11 @@ fn solve(path: []const u8) anyerror!usize {
 }
 
 fn example1() anyerror!usize {
-    return solve("problems/example_09.txt");
+    return solve("problems/example_09.txt", 2);
 }
 
 fn part1() anyerror!usize {
-    return solve("problems/problem_09.txt");
+    return solve("problems/problem_09.txt", 2);
 }
 
 test "example1" {
