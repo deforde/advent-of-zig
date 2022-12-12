25928265b891072850f165dd04fd02715e431b9c
deforde <daniel.forde001@gmail.com>
2022-12-12 12:45:58 +0200

    use zig build to build and test solutions

---
+++ src/main.zig
@@ -0,0 +1,14 @@
+test {
+    _ = @import("problem_01.zig");
+    _ = @import("problem_02.zig");
+    _ = @import("problem_03.zig");
+    _ = @import("problem_04.zig");
+    _ = @import("problem_05.zig");
+    _ = @import("problem_06.zig");
+    _ = @import("problem_07.zig");
+    _ = @import("problem_08.zig");
+    _ = @import("problem_09.zig");
+    _ = @import("problem_10.zig");
+    _ = @import("problem_11.zig");
+    _ = @import("problem_12.zig");
+}
--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-IFS=$'\n\t'
-
-SCRIPT_DIR=$(realpath ${0%/*})
-cd $SCRIPT_DIR
-
-find src -name "problem_*.zig" | awk '{printf("%s:\n", $0); system("zig test "$0); printf("\n")}'
