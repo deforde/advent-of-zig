12969b75a464e4c7ade1b1a28f087bf6e4b6354a
deforde <daniel.forde001@gmail.com>
2022-12-19 10:22:42 +0200

    fix sed command for main.zig

--- init.sh
+++ init.sh
@@ -9,5 +9,5 @@ touch problems/problem_$1.txt
 touch problems/example_$1.txt
 cp src/template src/problem_$1.zig
 sed -i "17s/example_01/example_$1/" src/problem_$1.zig
-NLINES=$(wc -l src/main.zig)
-sed -i "21s/.*/    _ = @import(\"problem_$1.zig\");\n}/" src/main.zig
+NLINES=$(wc -l src/main.zig | awk '{print $1}')
+sed -i "${NLINES}s/.*/    _ = @import(\"problem_$1.zig\");\n}/" src/main.zig
