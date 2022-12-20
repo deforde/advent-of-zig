75b7d9b5e48f59dfc97125eec17860eb90a00c4b
deforde <daniel.forde001@gmail.com>
2022-12-20 09:00:14 +0200

    tidy up solution

--- src/problem_20.zig
+++ src/problem_20.zig
@@ -30,20 +30,6 @@ fn printList(l: NodeList) void {
     std.debug.print("{}\n", .{n.x});
 }
 
-fn fixMix(l: *NodeList) void {
-    var n = l.head.?;
-    while (n != l.tail.?) {
-        var tmp = n.next.?;
-        n.next = n.mnext;
-        n.prev = n.mprev;
-        n = tmp;
-    }
-    n.next = n.mnext;
-    n.prev = n.mprev;
-    l.head = l.mhead;
-    l.tail = l.mtail;
-}
-
 fn doMix(l: *NodeList, n: *Node, len: usize) anyerror!void {
     const x = n.x;
     if (x != 0) {
@@ -60,14 +46,11 @@ fn doMix(l: *NodeList, n: *Node, len: usize) anyerror!void {
         var ins = mprev;
         const inc = @intCast(usize, try std.math.absInt(x)) % (len - 1);
 
-        if (x > 0) {
-            var i: usize = 0;
-            while (i < inc) : (i += 1) {
+        var i: usize = 0;
+        while (i < inc) : (i += 1) {
+            if (x > 0) {
                 ins = ins.mnext.?;
-            }
-        } else {
-            var i: usize = 0;
-            while (i < inc) : (i += 1) {
+            } else {
                 ins = ins.mprev.?;
             }
         }
@@ -94,7 +77,7 @@ fn mixList(l: *NodeList, len: usize) anyerror!void {
     try doMix(l, n, len);
 }
 
-fn solve1(path: []const u8) anyerror!i64 {
+fn solve(path: []const u8, decrypt_key: i64, nmix: usize) anyerror!i64 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
     defer std.debug.assert(!gpa.deinit());
@@ -105,69 +88,6 @@ fn solve1(path: []const u8) anyerror!i64 {
     var store = std.ArrayList(Node).init(allocator);
     defer store.deinit();
     try store.ensureTotalCapacity(5000);
-
-    var prev: ?*Node = null;
-
-    var list = NodeList{};
-
-    var lines = std.mem.tokenize(u8, buf, "\n");
-    while (lines.next()) |line| {
-        const x = try std.fmt.parseInt(i64, line, 10);
-        try store.append(Node{});
-        var n = &store.items[store.items.len - 1];
-        n.x = x;
-        n.prev = prev;
-        n.mprev = prev;
-        if (prev != null) {
-            prev.?.next = n;
-            prev.?.mnext = n;
-        } else {
-            list.head = n;
-            list.mhead = n;
-        }
-        list.tail = n;
-        list.mtail = n;
-        prev = n;
-    }
-
-    prev.?.next = &store.items[0];
-    prev.?.mnext = &store.items[0];
-    store.items[0].prev = prev;
-    store.items[0].mprev = prev;
-
-    // printList(list);
-    try mixList(&list, store.items.len);
-    // printList(list);
-
-    var sum: i64 = 0;
-    var n = list.mhead;
-    while (n.?.x != 0) : (n = n.?.mnext) {}
-    n = n.?.mnext;
-    var i: usize = 0;
-    while (i < 3000) : (i += 1) {
-        if ((i + 1) % 1000 == 0) {
-            // std.debug.print("\n{}\n", .{n.?.x});
-            sum += n.?.x;
-        }
-        n = n.?.mnext;
-    }
-
-    return sum;
-}
-
-fn solve2(path: []const u8) anyerror!i64 {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
-    defer std.debug.assert(!gpa.deinit());
-
-    const buf = try readFileIntoBuf(allocator, path);
-    defer allocator.free(buf);
-
-    var store = std.ArrayList(Node).init(allocator);
-    defer store.deinit();
-    try store.ensureTotalCapacity(5000);
-
-    const decrypt_key: i64 = 811589153;
 
     var prev: ?*Node = null;
 
@@ -199,8 +119,8 @@ fn solve2(path: []const u8) anyerror!i64 {
     store.items[0].mprev = prev;
 
     // printList(list);
-    var j: i64 = 0;
-    while (j < 10) : (j += 1) {
+    var j: usize = 0;
+    while (j < nmix) : (j += 1) {
         try mixList(&list, store.items.len);
     }
     // printList(list);
@@ -222,19 +142,19 @@ fn solve2(path: []const u8) anyerror!i64 {
 }
 
 fn example1() anyerror!i64 {
-    return solve1("problems/example_20.txt");
+    return solve("problems/example_20.txt", 1, 1);
 }
 
 fn example2() anyerror!i64 {
-    return solve2("problems/example_20.txt");
+    return solve("problems/example_20.txt", 811589153, 10);
 }
 
 fn part1() anyerror!i64 {
-    return solve1("problems/problem_20.txt");
+    return solve("problems/problem_20.txt", 1, 1);
 }
 
 fn part2() anyerror!i64 {
-    return solve2("problems/problem_20.txt");
+    return solve("problems/problem_20.txt", 811589153, 10);
 }
 
 test "example1" {
