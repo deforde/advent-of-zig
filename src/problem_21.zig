ea852714b3753afd53f5d7c86acd5923e548c464
deforde <daniel.forde001@gmail.com>
2022-12-21 15:45:57 +0200

    tidy up solution

--- src/problem_21.zig
+++ src/problem_21.zig
@@ -7,7 +7,6 @@ const NodeTy = enum {
     SUB,
     MUL,
     DIV,
-    EQL,
 };
 
 const Node = struct {
@@ -18,8 +17,10 @@ const Node = struct {
     v: i64 = 0,
 };
 
-fn findNode(store: *std.ArrayList(Node), nm: []const u8) *Node {
-    for (store.items) |*n| {
+const BackingStore = [2587]Node;
+
+fn findNode(store: *BackingStore, nm: []const u8) *Node {
+    for (store) |*n| {
         if (std.mem.eql(u8, n.nm, nm)) {
             return n;
         }
@@ -27,29 +28,71 @@ fn findNode(store: *std.ArrayList(Node), nm: []const u8) *Node {
     unreachable;
 }
 
-fn calcNode(n: *Node, delta: ?*i64, rev: ?bool) i64 {
+fn calcNode(n: *Node) i64 {
     if (n.ty == NodeTy.NUM) {
         return n.v;
     }
-    const l = calcNode(n.l.?, null, null);
-    const r = calcNode(n.r.?, null, null);
+    const l = calcNode(n.l.?);
+    const r = calcNode(n.r.?);
     switch (n.ty) {
         NodeTy.ADD => return l + r,
         NodeTy.SUB => return l - r,
         NodeTy.MUL => return l * r,
         NodeTy.DIV => return @divTrunc(l, r),
-        NodeTy.EQL => {
-            if (delta != null) {
-                if (rev.?) {
-                    delta.?.* = r - l;
-                } else {
-                    delta.?.* = l - r;
-                }
-            }
-            // std.debug.print("{} == {}\n", .{ l, r });
-            return if (l == r) 1 else 0;
-        },
         else => unreachable,
+    }
+}
+
+fn initNodes(buf: []const u8, store: *BackingStore, root: *?*Node) anyerror!void {
+    var idx: usize = 0;
+
+    var lines = std.mem.tokenize(u8, buf, "\n");
+    while (lines.next()) |line| {
+        var records = std.mem.tokenize(u8, line, " :");
+        const nm = records.next().?;
+        const op1 = records.next().?;
+        if (op1[0] >= '0' and op1[0] <= '9') {
+            const n = Node{
+                .nm = nm,
+                .ty = NodeTy.NUM,
+                .v = try std.fmt.parseInt(i64, op1, 10),
+            };
+            store[idx] = n;
+            idx += 1;
+        } else {
+            const op = records.next().?;
+            const nty = switch (op[0]) {
+                '+' => NodeTy.ADD,
+                '-' => NodeTy.SUB,
+                '*' => NodeTy.MUL,
+                '/' => NodeTy.DIV,
+                else => unreachable,
+            };
+            const n = Node{
+                .nm = nm,
+                .ty = nty,
+            };
+            store[idx] = n;
+            idx += 1;
+        }
+        if (std.mem.eql(u8, nm, "root")) {
+            root.* = &store[idx - 1];
+        }
+    }
+
+    lines.reset();
+    while (lines.next()) |line| {
+        var records = std.mem.tokenize(u8, line, " :");
+        const nm = records.next().?;
+        const op1 = records.next().?;
+        if (op1[0] >= '0' and op1[0] <= '9') {
+            continue;
+        }
+        _ = records.next().?;
+        const op2 = records.next().?;
+        var n = findNode(store, nm);
+        n.l = findNode(store, op1);
+        n.r = findNode(store, op2);
     }
 }
 
@@ -61,63 +104,14 @@ fn solve1(path: []const u8) anyerror!i64 {
     const buf = try readFileIntoBuf(allocator, path);
     defer allocator.free(buf);
 
-    var store = std.ArrayList(Node).init(allocator);
-    defer store.deinit();
-    try store.ensureTotalCapacity(2587);
-
     var root: ?*Node = null;
+    var store = std.mem.zeroes(BackingStore);
+    try initNodes(buf, &store, &root);
 
-    var lines = std.mem.tokenize(u8, buf, "\n");
-    while (lines.next()) |line| {
-        var records = std.mem.tokenize(u8, line, " :");
-        const nm = records.next().?;
-        const op1 = records.next().?;
-        if (op1[0] >= '0' and op1[0] <= '9') {
-            const n = Node{
-                .nm = nm,
-                .ty = NodeTy.NUM,
-                .v = try std.fmt.parseInt(i64, op1, 10),
-            };
-            try store.append(n);
-        } else {
-            const op = records.next().?;
-            const nty = switch (op[0]) {
-                '+' => NodeTy.ADD,
-                '-' => NodeTy.SUB,
-                '*' => NodeTy.MUL,
-                '/' => NodeTy.DIV,
-                else => unreachable,
-            };
-            const n = Node{
-                .nm = nm,
-                .ty = nty,
-            };
-            try store.append(n);
-        }
-        if (std.mem.eql(u8, nm, "root")) {
-            root = &store.items[store.items.len - 1];
-        }
-    }
-
-    lines.reset();
-    while (lines.next()) |line| {
-        var records = std.mem.tokenize(u8, line, " :");
-        const nm = records.next().?;
-        const op1 = records.next().?;
-        if (op1[0] >= '0' and op1[0] <= '9') {
-            continue;
-        }
-        _ = records.next().?;
-        const op2 = records.next().?;
-        var n = findNode(&store, nm);
-        n.l = findNode(&store, op1);
-        n.r = findNode(&store, op2);
-    }
-
-    return calcNode(root.?, null, null);
+    return calcNode(root.?);
 }
 
-fn solve2(path: []const u8, init_max: i64, rev: bool) anyerror!i64 {
+fn solve2(path: []const u8) anyerror!i64 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
     defer std.debug.assert(!gpa.deinit());
@@ -125,72 +119,35 @@ fn solve2(path: []const u8, init_max: i64, rev: bool) anyerror!i64 {
     const buf = try readFileIntoBuf(allocator, path);
     defer allocator.free(buf);
 
-    var store = std.ArrayList(Node).init(allocator);
-    defer store.deinit();
-    try store.ensureTotalCapacity(2587);
-
     var root: ?*Node = null;
+    var store = std.mem.zeroes(BackingStore);
+    try initNodes(buf, &store, &root);
 
-    var lines = std.mem.tokenize(u8, buf, "\n");
-    while (lines.next()) |line| {
-        var records = std.mem.tokenize(u8, line, " :");
-        const nm = records.next().?;
-        const op1 = records.next().?;
-        if (op1[0] >= '0' and op1[0] <= '9') {
-            const n = Node{
-                .nm = nm,
-                .ty = NodeTy.NUM,
-                .v = try std.fmt.parseInt(i64, op1, 10),
-            };
-            try store.append(n);
-        } else {
-            const op = records.next().?;
-            const nty = switch (op[0]) {
-                '+' => NodeTy.ADD,
-                '-' => NodeTy.SUB,
-                '*' => NodeTy.MUL,
-                '/' => NodeTy.DIV,
-                else => unreachable,
-            };
-            const n = Node{
-                .nm = nm,
-                .ty = nty,
-            };
-            try store.append(n);
-        }
-        if (std.mem.eql(u8, nm, "root")) {
-            root = &store.items[store.items.len - 1];
-            root.?.ty = NodeTy.EQL;
-        }
-    }
+    root.?.ty = NodeTy.SUB;
 
-    lines.reset();
-    while (lines.next()) |line| {
-        var records = std.mem.tokenize(u8, line, " :");
-        const nm = records.next().?;
-        const op1 = records.next().?;
-        if (op1[0] >= '0' and op1[0] <= '9') {
-            continue;
-        }
-        _ = records.next().?;
-        const op2 = records.next().?;
-        var n = findNode(&store, nm);
-        n.l = findNode(&store, op1);
-        n.r = findNode(&store, op2);
-    }
-
-    var delta: i64 = std.math.maxInt(i64);
     var min: i64 = 0;
-    var max = init_max;
+    var max: i64 = 1000000000000000;
     var n = findNode(&store, "humn");
     n.v = max;
-    while (calcNode(root.?, &delta, rev) == 0) : (n.v = @divTrunc(max - min, 2) + min) {
-        if (delta < 0) {
-            min = n.v;
-        } else {
+
+    if (calcNode(root.?) < 0) {
+        var tmp = root.?.l;
+        root.?.l = root.?.r;
+        root.?.r = tmp;
+    }
+
+    while (true) {
+        n.v = @divTrunc(max - min, 2) + min;
+        var delta = calcNode(root.?);
+        if (delta == 0) {
+            while (calcNode(root.?) == 0) : (n.v -= 1) {}
+            n.v += 1;
+            break;
+        } else if (delta > 0) {
             max = n.v;
+        } else {
+            min = n.v;
         }
-        // std.debug.print("{} - {}, {}, {}\n", .{ min, max, n.v, delta });
     }
 
     return n.v;
@@ -201,7 +158,7 @@ fn example1() anyerror!i64 {
 }
 
 fn example2() anyerror!i64 {
-    return solve2("problems/example_21.txt", 100000, false);
+    return solve2("problems/example_21.txt");
 }
 
 fn part1() anyerror!i64 {
@@ -209,7 +166,7 @@ fn part1() anyerror!i64 {
 }
 
 fn part2() anyerror!i64 {
-    return solve2("problems/problem_21.txt", 1000000000000000, true);
+    return solve2("problems/problem_21.txt");
 }
 
 test "example1" {
