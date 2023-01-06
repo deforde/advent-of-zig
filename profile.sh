deab047409e1870b972d9eb5dc045feea5e8cfbe
deforde <daniel.forde001@gmail.com>
2023-01-06 14:57:53 +0200

    fix profiling script

--- profile.sh
+++ profile.sh
@@ -10,24 +10,21 @@ PROBLEM_SRCS=$(find src -name "problem_*" | sort -r)
 rm -rf profile.txt
 
 for PROBLEM_SRC in $PROBLEM_SRCS; do
-    echo $PROBLEM_SRC
+    printf "%s\n" $PROBLEM_SRC &>> profile.txt
 
-    echo part1
+    printf "%s\n" part1 &>> profile.txt
     rm -rf zig-cache
     zig test --test-no-exec --test-filter part1 -O ReleaseFast $PROBLEM_SRC
     find zig-cache -name test | \
     awk '{print $NF}' | \
-    xargs time -f "%E" >> profile.txt
+    xargs time -f "%E" &>> profile.txt
 
-    echo part2
+    printf "%s\n" part2 &>> profile.txt
     rm -rf zig-cache
     zig test --test-no-exec --test-filter part2 -O ReleaseFast $PROBLEM_SRC
     find zig-cache -name test | \
     awk '{print $NF}' | \
-    xargs time -f "%E" >> profile.txt
-
-    echo
+    xargs time -f "%E" &>> profile.txt
 done
 
 awk '/src\/problem_/ { split($0, a, "_"); split(a[2], a, "."); problem = "problem " + a[1]; } /^part/ { part = $0; } /^[0-9]/ { print problem " " part " " $0; }' profile.txt | sort -r -k3
-rm -rf profile.txt
